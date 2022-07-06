2ed8d43e8861a2ab50b4e2df4742d65b90560e26
comicodarko <bitotajima@hotmail.com>
2022-07-06 10:10:16 -0300

    🚀 - Configurações para deploy.

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = process.env.REACT_APP_API_URL;
+export const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 axios.defaults.baseURL = baseUrl;
 
 export async function getCategories() {
